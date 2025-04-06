1520016de0d15242d4b38750258993ccc4d32378
Anuj <147524186+AnujKr028@users.noreply.github.com>
2025-04-06 10:22:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 from flask import Flask, request, jsonify
 import joblib
 import numpy as np
+from flask_cors import CORS
+
 
 app = Flask(__name__)
+CORS(app) 
 
 # Load the model
 model = joblib.load("model.pkl")
